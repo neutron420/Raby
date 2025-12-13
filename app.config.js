4414d5b978921b2857ff74b1a9801b2f8c2958df
neutron420 <rihankumar2004@gmail.com>
2025-12-13 23:10:45 +0530

    commit

--- app.config.js
+++ app.config.js
@@ -2,7 +2,22 @@ import 'dotenv/config';
 
 export default ({ config }) => ({
   ...config,
+
+  plugins: [
+    ...(config.plugins ?? []),
+    "expo-barcode-scanner",
+  ],
+
+  android: {
+    ...config.android,
+    package: "com.ritesh004.raby",
+  },
+
   extra: {
+    ...config.extra,
     INFURA_API_KEY: process.env.INFURA_API_KEY,
+    eas: {
+      projectId: "3c173e9d-7a07-4e9f-8dfe-9b9253c48e34",
+    },
   },
 });
